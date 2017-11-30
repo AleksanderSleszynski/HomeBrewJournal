7321b0f3eef6429e9ad99c85bd2781dc44cab2d8
Rasmuhamed <Rasmuhamed@users.noreply.github.com>
2017-11-30 12:02:50 +0100

    Edit Beer Object

--- app/src/main/java/com/example/julian/homebrewjournal/model/Beer.kt
+++ app/src/main/java/com/example/julian/homebrewjournal/model/Beer.kt
@@ -1,3 +1,4 @@
 package com.example.julian.homebrewjournal.model
 
-data class Beer(val name: String, val style: String)
+data class Beer(val name: String, val style: String, val finalGravity: Double,
+                val originalGravity: Double, val beerVolume: Double,  val boilVolume: Double)
